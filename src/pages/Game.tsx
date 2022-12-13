ac1332fcd310bb7e87de8dbbf2939413a3d5459f
Aleksa Petrovic <aleksapetrovic99@gmail.com>
2022-12-13 20:26:27 +0100

    added modal on start to show news

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,7 +1,9 @@
-import { Box, Text, useDisclosure } from "@chakra-ui/react";
+import { Box, Checkbox, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "../features/nav/Nav";
 import Topbar from "../features/nav/topbar/Topbar";
+import PrimaryButton from "../util/components/PrimaryButton";
 
 const Game = () => {
   const {
@@ -9,6 +11,21 @@ const Game = () => {
     onOpen: onSidebarOpen,
     onClose: onSidebarClose,
   } = useDisclosure();
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [dontShowNewsChecked, setDontShowNewsChecked] = useState<boolean>(false);
+  useEffect(() => {
+    const dontShowNews = localStorage.getItem("dontShowNews");
+    if (dontShowNews !== "true") {
+      onOpen();
+    }
+  }, [])
+
+  const handleNewsOk = () => {
+    if (dontShowNewsChecked) {
+      localStorage.setItem("dontShowNews", "true")
+    }
+    onClose();
+  }
 
   return (
     <Box>
@@ -20,13 +37,26 @@ const Game = () => {
           onSidebarClose={onSidebarClose}
         />
       </nav>
-      <Box m={4}>
-        <Text color="red" fontSize="2xl">Project still in development</Text>
-        <Text color="gray.300">Check out the combat, woodcutting and mining pages</Text>
-      </Box>
       <Box>
         <Outlet />
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader borderTopRadius={15} color="gray.300" bg="#3a3b47">News</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody color="gray.300" bg={"#3c4b64"}>
+            <Text color="red" fontSize="2xl">Project still in development</Text>
+            <Text color="gray.300">Check out the inventory, combat, traveling, woodcutting and mining pages</Text>
+          </ModalBody>
+
+          <ModalFooter borderBottomRadius={15} bg="#3a3b47">
+            <Checkbox onChange={(e) => setDontShowNewsChecked(e.target.checked)} color="gray.300">Dont show again</Checkbox>
+            <PrimaryButton ml={3} text="Ok" onClick={handleNewsOk} />
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
--- src/util/components/PrimaryButton.tsx
+++ src/util/components/PrimaryButton.tsx
@@ -1,11 +1,12 @@
 import { Box, Button } from '@chakra-ui/react'
 
-const PrimaryButton = ({ text, size, onClick, mr, disabled, children }: any) => {
+const PrimaryButton = ({ text, size, onClick, mr, ml, disabled, children }: any) => {
   return (
     <Button bg="orange.400"
       color="white"
       size={size}
       mr={mr}
+      ml={ml}
       disabled={disabled}
       onClick={() => onClick()}
     >
